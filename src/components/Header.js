07501652360083cdc01954feeb00745ac543dab2
lauraarango14 <lauraarango_14@outlook.com>
2023-05-24 17:03:36 -0500

    Nuevo

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import './Header.scss';
+import {Menu} from 'semantic-ui-react';
+
+export default function Header() {
+ return (
+    <div className="header-menu">
+        <Menu secondary>
+            <Menu.Item name="inicio" onClick={()=>console.log("haciendo clic en el menu")} />
+            <Menu.Item name="series" onClick={()=>console.log("haciendo clic en series")} />
+            <Menu.Item name="comics" onClick={()=>console.log("haciendo clic en comics")} />
+        </Menu>
+    </div>
+
+ );   
+}
